410369ab254d9371ee0a9655670cad1b11ed39d6
jocelynshen <jocelyn.j.shen@gmail.com>
2018-09-15 17:52:48 -0400

    upload image

--- www/main.js
+++ www/main.js
@@ -13,6 +13,21 @@ $('.dots li').click(function(){
   $(this).addClass('active');
 });
 
+$("input").change(function(e) {
+
+    for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+        
+        var file = e.originalEvent.srcElement.files[i];
+        
+        var img = document.createElement("img");
+        var reader = new FileReader();
+        reader.onloadend = function() {
+             img.src = reader.result;
+        }
+        reader.readAsDataURL(file);
+        $("input").after(img);
+    }
+});
 
 function onClick(element) {
   document.getElementById("img01").src = element.src;
