a9f31bda14efc999be3f221e678fb2e79e50bdf4
Dong-Gil Shin <dgshin@mit.edu>
2018-09-16 03:06:05 -0400

    fixed bug?

--- platforms/browser/www/maintemp.js
+++ platforms/browser/www/maintemp.js
@@ -60,7 +60,6 @@ function main()
     })
 }
 
-
 function myFunction(memoizeRef){
     var x = document.getElementById("file");
     var txt = "";
@@ -71,16 +70,13 @@ function myFunction(memoizeRef){
             for (var i = 0; i < x.files.length; i++) {
                 txt += "<br><strong>" + (i+1) + ". file</strong><br>";
                 var file = x.files[i];
-                console.log("filenameis")
-                console.log(file)
                 //
-                memoizeRef.child(file.name).put(file);
+                memoizeRef.child("image.png").put(file);
                 console.log(memoizeRef.name);
                 console.log(memoizeRef.fullPath);
                 console.log(memoizeRef);
                 console.log(file);
                 //
-                retrieveImageUrl(memoizeRef, file.name);
 
                 if ('name' in file) {
                     txt += "name: " + file.name + "<br>";
@@ -103,25 +99,70 @@ function loadImageFileAsURL(e)
 
         if (fileToLoad.type.match("image.*"))
         {
+            var fileReader = new FileReader();
+            fileReader.onload = function(fileLoadedEvent) 
+            {
+                var imageLoaded = document.createElement("img");
+                var x = document.createElement("HEADER");
+                x.setAttribute("id", "myHeader");
+                x.setAttribute("class", "w3-center")
+                imageLoaded.src = fileLoadedEvent.target.result;
+                imageLoaded.className = "w3-image w3-padding-large w3-hover-opacity";
+                document.getElementById("chatDisplay").appendChild(x);
+                var today = new Date();
+                var dd = today.getDate();
+                var mm = today.getMonth()+1; //January is 0!
+                var yyyy = today.getFullYear();
+                if(dd<10) {
+                    dd = '0'+dd
+                } 
+                if(mm<10) {
+                    mm = '0'+mm
+                } 
+                today = mm+ '/' + dd + '/' + yyyy;
+                var y = document.createElement("H3"); 
+                var t = document.createTextNode(today);
+                y.appendChild(t);
+                if(myHeaders.includes(today)){
+                    
+                }
+                else{
+                    document.getElementById("myHeader").appendChild(y);
+                    myHeaders.push(today);
+                }
+                imageLoaded.onclick = onClick(imageLoaded);
+                imageLoaded.setAttribute("onclick", "onClick(this)")
+                document.getElementById("chatDisplay").appendChild(imageLoaded);
+                //document.body.appendChild(imageLoaded);
+                //document.getElementById("chatDisplay").innerHTML = "";
+
+                
+                
+            }
+            fileReader.readAsDataURL(fileToLoad);
             myFunction(memoizeRef);
-            
+            retrieveImageUrl(memoizeRef);
         }
     }
 }
-function retrieveImageUrl(memoizeRef, filename){
+
+function clearChatDisplay(){
+    document.getElementById("chatDisplay").innerHTML = "";
+}
+function retrieveImageUrl(memoizeRef){
      /*
             var storageRef = firebase.storage().ref();
             var spaceRef = storageRef.child('images/photo_1.png');
             var path = spaceRef.fullPath;
             var gsReference = storage.refFromURL('gs://test.appspot.com')
             */
-            memoizeRef.child(filename).getDownloadURL().then(function(url) {
+            memoizeRef.child('image.png').getDownloadURL().then(function(url) {
               var test = url;
               console.log(test)
               var imageLoaded = document.createElement("img");
             imageLoaded.src = test;
             imageLoaded.className = "w3-image w3-padding-large w3-hover-opacity";
-            document.body.appendChild(imageLoaded);
+            document.getElementById("chatDisplay").appendChild(imageLoaded);
             }).catch(function(error) {
 
             });
