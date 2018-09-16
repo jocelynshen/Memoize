68a15a03d3d9cd560260cca239e13b956ecda40b
jocelynshen <jocelyn.j.shen@gmail.com>
2018-09-16 02:18:13 -0400

    uploading and retreiving

--- www/main.js
+++ www/main.js
@@ -21,9 +21,6 @@ function initializeFirebase(){
 
 }
 
-
-
-
 function addChat(db, chatWith, owner, emotion, screenURL){
   db.collection("chats").add({
                 chatWith: chatWith,
@@ -55,7 +52,6 @@ function onClick(element) {
   captionText.innerHTML = element.alt;
 }
 
-
 function main()
 {
     $(function(){
@@ -63,6 +59,36 @@ function main()
         picker.addEventListener('change', loadImageFileAsURL)
     })
 }
+
+function myFunction(memoizeRef){
+    var x = document.getElementById("file");
+    var txt = "";
+    if ('files' in x) {
+        if (x.files.length == 0) {
+            txt = "Select one or more files.";
+        } else {
+            for (var i = 0; i < x.files.length; i++) {
+                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+                var file = x.files[i];
+                //
+                memoizeRef.child("image.png").put(file);
+                console.log(memoizeRef.name);
+                console.log(memoizeRef.fullPath);
+                console.log(memoizeRef);
+                console.log(file);
+                //
+
+                if ('name' in file) {
+                    txt += "name: " + file.name + "<br>";
+                }
+                if ('size' in file) {
+                    txt += "size: " + file.size + " bytes <br>";
+                }
+            }
+        }
+    } 
+}
+
 var myHeaders =[];
 function loadImageFileAsURL(e)
 {
@@ -73,45 +99,8 @@ function loadImageFileAsURL(e)
 
         if (fileToLoad.type.match("image.*"))
         {
-            var fileReader = new FileReader();
-            fileReader.onload = function(fileLoadedEvent) 
-            {
-                var imageLoaded = document.createElement("img");
-                var x = document.createElement("HEADER");
-                x.setAttribute("id", "myHeader");
-                x.setAttribute("class", "w3-center")
-                imageLoaded.src = fileLoadedEvent.target.result;
-                imageLoaded.className = "w3-image w3-padding-large w3-hover-opacity";
-                document.body.appendChild(x);
-                var today = new Date();
-                var dd = today.getDate();
-                var mm = today.getMonth()+1; //January is 0!
-                var yyyy = today.getFullYear();
-                if(dd<10) {
-                    dd = '0'+dd
-                } 
-                if(mm<10) {
-                    mm = '0'+mm
-                } 
-                today = mm+ '/' + dd + '/' + yyyy;
-                var y = document.createElement("H3"); 
-                var t = document.createTextNode(today);
-                y.appendChild(t);
-                if(myHeaders.includes(today)){
-                    
-                }
-                else{
-                    document.getElementById("myHeader").appendChild(y);
-                    myHeaders.push(today);
-                }
-                imageLoaded.onclick = onClick(imageLoaded);
-                imageLoaded.setAttribute("onclick", "onClick(this)")
-                document.body.appendChild(imageLoaded);
-                
-                
-                
-            }
-            fileReader.readAsDataURL(fileToLoad);
+            myFunction(memoizeRef);
+            retrieveImageUrl(memoizeRef);
         }
     }
 }
@@ -124,7 +113,6 @@ function retrieveImageUrl(memoizeRef){
             */
             memoizeRef.child('image.png').getDownloadURL().then(function(url) {
               var test = url;
-              console.log("aaa")
               console.log(test)
               var imageLoaded = document.createElement("img");
             imageLoaded.src = test;
@@ -133,47 +121,8 @@ function retrieveImageUrl(memoizeRef){
             }).catch(function(error) {
 
             });
-            
-            //console.log(test)
-
 }
 
-
-
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-    var user=getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-       user = prompt("Please enter your name:","");
-       if (user != "" && user != null) {
-           setCookie("username", user, 30);
-       }
-    }
-}
 main();
 
 
